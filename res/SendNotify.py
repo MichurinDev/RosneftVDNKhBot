a25e296d8422091510f006b6fc4e9dd57187d375
MichurinDev <andreymithurin00@gmail.com>
2023-11-19 00:57:49 +0700

    Update 2

--- res/SendNotify.py
+++
@@ -1,9 +0,0 @@
-from notifiers import get_notifier
-
-
-def send_notify(token: str, msg: str, chatId: int):
-    telegram = get_notifier('telegram')
-    telegram.notify(
-        token=token,
-        chat_id=chatId,
-        message=msg)
