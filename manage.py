dbfdf660b478085aadd3dee0bd75b3c78afaf665
MichurinDev <andreymithurin00@gmail.com>
2023-11-19 22:57:26 +0700

    Изменение менеджера

--- manage.py
+++ manage.py
@@ -5,14 +5,14 @@ from sys import platform
 
 def sturtup_client_bot():
     if platform == "linux" or platform == "linux2":
-        os.system("python3 res/main.py")
+        os.system("python3 ./res/main.py")
     elif platform == "win32":
         os.system("python ./res/main.py")
 
 
 def sturtup_admin_bot():
     if platform == "linux" or platform == "linux2":
-        os.system("python3 res/admin_bot.py")
+        os.system("python3 ./res/admin_bot.py")
     elif platform == "win32":
         os.system("python ./res/admin_bot.py")
 
